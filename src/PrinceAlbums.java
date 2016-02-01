c08e97614dac406f4296e77d1b62e5bc478f8a6c
mariamummert <mlbibbs@gmail.com>
2016-02-01 13:09:54 -0500

    Prince Album Info

---
+++ src/PrinceAlbums.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mariamummert on 2/1/16.
+ */
+public class PrinceAlbums {
+    public String title;
+    public int year;
+    public boolean platinum;
+    public String label;
+}
+Prince
