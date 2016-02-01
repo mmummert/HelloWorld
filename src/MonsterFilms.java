7ebe3552b624c78f4920959b80e2d4178dfba282
mariamummert <mlbibbs@gmail.com>
2016-02-01 13:55:23 -0500

    Monster Movie list for Halloween

---
+++ src/MonsterFilms.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mariamummert on 2/1/16.
+ */
+public class MonsterFilms {
+    public String name;
+    public int [] ReleaseYear;
+    public boolean BlackAndWhite;
+    public boolean domestic;
+    public double BoxOfficeGross;
+}
