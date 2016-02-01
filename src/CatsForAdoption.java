219dd4100e3446a275f04a3a00ede2c824cf582d
mariamummert <mlbibbs@gmail.com>
2016-02-01 13:15:43 -0500

    Adoption information for cat rescue

---
+++ src/CatsForAdoption.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mariamummert on 2/1/16.
+ */
+public class CatsForAdoption {
+    public String name;
+    public int age;
+    public boolean microchipped;
+    public double cost;
+
+}
